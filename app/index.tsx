b8a4ffcb9d0faea90bdfac8876b928489e75389d
from madafa <nassoromasiku255@gmail.com>
2026-07-05 05:28:33 +0300

    mhh

--- app/index.tsx
+++ app/index.tsx
@@ -6,12 +6,12 @@ import { scaleFont, scaleHeight, scaleWidth } from './thyscale'
 export default function thst(){
     const seeFun=()=>{
         return(
-            <View>
-                <View style={{borderRadius:scaleHeight(10), overflow:"hidden",marginTop:scaleHeight(440), marginLeft:scaleWidth(50)}}>
+            <View style={{alignItems:"center", justifyContent:"center"}}>
+                <View style={{borderRadius:scaleHeight(10), overflow:"hidden", marginTop:scaleHeight(40)}}>
                     <Image source={require("../assets/images/thyicn1.png")} style={{height:scaleHeight(200),width:scaleWidth(200)}}></Image> 
                 </View>    
-                    <Text style={{fontSize:scaleFont(34), fontWeight:"bold",color:"rgb(51, 102, 255)", marginLeft:scaleWidth(50), marginTop:scaleHeight(10)}}>DataCollect</Text>
-                <Text style={{paddingTop:scaleHeight(20), fontSize:scaleFont(20), marginLeft:scaleWidth(-20)}}>Developed by Kitaraju Mgonja</Text>
+                <Text style={{fontSize:scaleFont(34), fontWeight:"bold",color:"rgb(51, 102, 255)", marginTop:scaleHeight(10)}}>DataCollect</Text>
+                <Text style={{paddingTop:scaleHeight(20), fontSize:scaleFont(20)}}>Developed by Kitaraju Mgonja</Text>
             </View>
         )
     }
@@ -24,7 +24,7 @@ export default function thst(){
         
     
     return(
-        <View style={{marginLeft:scaleWidth(220)}}>
+        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
             {seeFun()}
         </View>
     )
